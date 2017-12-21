1a5ddad82dc9cbfdce40cb6421542db67c14ca36
Aditya Ramteke <adityaramteke05icr@gmail.com>
2017-12-21 16:02:06 +0530

    changed oneclicked to onclicked.

--- christmastree.py
+++ christmastree.py
@@ -1,5 +1,8 @@
 import turtle
 
+colors_one = ['red', 'yellow']
+colors_two = ['yellow', 'red']
+
 turtle.bgcolor("black")
 screen = turtle.Screen()
 screen.setup(800,600)
@@ -18,6 +21,36 @@ square.up()
 
 circle.goto(0,280)
 circle.stamp()
+
+
+def blinking(changed_color_one, changed_color_two):
+    circle.goto(0,280)
+    circle.color(changed_color_two)
+    circle.stamp()
+    k = 0
+    for i in range(1, 17):
+        y = 30*i
+        for j in range(i-k):
+            x = 30*j
+
+        if i % 4 == 0:
+            x =  30*(j+1)
+            circle.color(changed_color_one)
+            circle.goto(-x,-y+280)
+            circle.stamp()
+            circle.goto(x,-y+280)
+            circle.stamp()
+            k += 2
+
+        if i % 4 == 3:
+            x =  30*(j+1)
+            circle.color(changed_color_two)
+            circle.goto(-x,-y+280)
+            circle.stamp()
+            circle.goto(x,-y+280)
+            circle.stamp()
+
+
 
 k = 0
 for i in range(1, 17):
@@ -55,5 +88,8 @@ for i in range(17,20):
         square.stamp()
         square.goto(-x,-y+280)
         square.stamp()        
-        
-turtle.done()
+
+for h in range(100):
+    blinking(colors_one[h%2], colors_two[h%2])
+
+turtle.onclick()
